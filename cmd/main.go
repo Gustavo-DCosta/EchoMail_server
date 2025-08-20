b2cc591ede179f6bcfcb3eca11b210dd218ebf6f
Gustavo-DCosta <gustagamerpereira@gmail.com>
2025-08-20 09:45:40 +0200

    removed supabase response json file from files

--- cmd/main.go
+++ cmd/main.go
@@ -11,15 +11,19 @@ import (
 )
 
 func init() {
+	// load env vars from the embedded .env file
 	gotenv.Load()
 }
 
 func main() {
+	// init connections
 	database.Connect_To_PostgreSQL()
 	database.Connect_To_Redis()
+
+	// setup router
 	router.Router()
 
-	fmt.Println("Server running...")
+	fmt.Println("Server running on 127.0.0.1:8080")
 	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
